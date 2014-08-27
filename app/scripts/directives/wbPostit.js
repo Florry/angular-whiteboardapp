7afeb9c814ca96e808c96db687007d445eff5d8e
florry <soderstrom.viktor@gmail.com>
2014-08-27 15:36:48 +0200

    added postit movment function

--- app/scripts/directives/wbPostit.js
+++ app/scripts/directives/wbPostit.js
@@ -7,17 +7,45 @@
  * # postIt
  */
 angular.module('whiteboardApp')
-	.directive('wbPostIt', function () {
+	.directive('wbPostIt', function ($document) {
 		return {
 			templateUrl: './scripts/directives/templates/post-it.html',
 			restrict: 'E',
 			scope: {
 				content: '='
 			},
-			controller: function postItCtrl($scope) {
+			link: function postItCtrl($scope, element) {
+				var startX = element.offset().left,
+					startY = element.offset().top,
+					x = 0,
+					y = 0;
+
 				$scope.getStatusCss = function () {
 					return $scope.content.status.replace(' ', '-');
 				};
+
+				(function bindElementMove() {
+					element.bind('mousedown', function (event) {
+						startX = event.screenX - element.offset().left;
+						startY = event.screenY - element.offset().top;
+						$document.bind('mousemove', movePostit);
+						$document.bind('mouseup', mouseup);
+					});
+				}());
+
+				function movePostit(event) {
+					y = event.screenY - startY;
+					x = event.screenX - startX;
+					element.css({
+						top: y + 'px',
+						left: x + 'px'
+					});
+				}
+
+				function mouseup() {
+					$document.unbind('mousemove', movePostit);
+					$document.unbind('mouseup', mouseup);
+				}
 			}
 		};
 	});
