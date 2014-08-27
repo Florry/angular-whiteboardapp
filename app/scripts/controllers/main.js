057557ff713a492323372856cc0ad86d43c228f7
iPrytz <isak.prytz@gmail.com>
2014-08-27 14:47:38 +0200

    updatePostIt added to CRUDFactory

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,7 +8,7 @@
  * Controller of the whiteboardApp
  */
 angular.module('whiteboardApp')
-	.controller('MainCtrl', function ($scope) {
+	.controller('MainCtrl', function($scope, CRUDFactory) {
 
 		$scope.postits = [{
 			id: 1,
@@ -99,4 +99,6 @@ angular.module('whiteboardApp')
 			removed: false,
 			timestamp: '2014-08-32'
 		}];
+
+
 	});
