ab5219587a8206268b81112794287d960b702b5b
Dannie Håkansson <danniehakan@gmail.com>
2014-09-01 14:46:43 +0200

    fixed bug with postit not being placed where ghost was

--- app/scripts/services/CRUDFactory.js
+++ app/scripts/services/CRUDFactory.js
@@ -1,42 +1,42 @@
 'use strict';
 
 angular.module('whiteboardApp')
-	.factory('CRUDFactory', function ($http) {
+	.factory('CRUDFactory', function($http) {
 
 		// C POST OBJ TO URL http://api.beta2.se/wb-div-postits
 		// R GET OBJ FROM URL http://api.beta2.se/wb-div-postits
 		// U PUT OBJ TO URL http://api.beta2.se/wb-div-postits/:id
 		// D DELETE OBJ TO URL http://api.beta2.se/wb-div-postits/:id
 
-		var URL = 'http://api.beta2.se/wb-div-postits',
+		var URL = 'http://localhost:14782/wb-div-postits',
 			postits = [];
 
 		return {
 			//C
-			createPostIt: function (newPostIt, callback) {
-				$http.post(URL, newPostIt).success(function (data) {
+			createPostIt: function(newPostIt, callback) {
+				$http.post(URL, newPostIt).success(function(data) {
 					callback(data);
 				});
 			},
 			//R
-			readPostIt: function (callback) {
-				$http.get(URL + '/').success(function (data) {
+			readPostIt: function(callback) {
+				$http.get(URL + '/').success(function(data) {
 					postits = data;
 					callback();
 				});
 			},
 			//U
-			updatePostIt: function (postIt) {
+			updatePostIt: function(postIt) {
 				$http.put(URL + '/' + postIt.id, postIt);
 				console.log(postIt + ' was updated on the server');
 			},
 			//D
-			deletePostIt: function (postIt) {
+			deletePostIt: function(postIt) {
 				$http.delete(URL + '/' + postIt);
 				console.log(postIt + ' was deleted from the server');
 			},
 
-			getPostIts: function () {
+			getPostIts: function() {
 				// console.log(postits);
 				return postits;
 			}
