5c3576c8b4eb235f9d678bbb03eccc8a3c7dde5d
florry <soderstrom.viktor@gmail.com>
2014-09-01 09:16:45 +0200

    removed populate button

--- app/scripts/services/CRUDFactory.js
+++ app/scripts/services/CRUDFactory.js
@@ -1,44 +1,44 @@
 'use strict';
 
 angular.module('whiteboardApp')
-	.factory('CRUDFactory', function($http) {
+	.factory('CRUDFactory', function ($http) {
 
 		// C POST OBJ TO URL http://api.beta2.se/wb-div-postits
 		// R GET OBJ FROM URL http://api.beta2.se/wb-div-postits
 		// U PUT OBJ TO URL http://api.beta2.se/wb-div-postits/:id
 		// D DELETE OBJ TO URL http://api.beta2.se/wb-div-postits/:id
 
-		var URL = 'http://localhost:14782/wb-div-postits',
+		var URL = 'http://api.beta2.se/wb-div-postits',
 			postits = [];
 
 		return {
 			//C
-			createPostIt: function(postIt) {
+			createPostIt: function (postIt) {
 				return $http.post(URL, postIt);
 			},
 			//R
-			readPostIt: function(callback) {
+			readPostIt: function (callback) {
 				$http({
 					method: 'GET',
 					url: URL + '/'
 				}).
-				success(function(data) {
+				success(function (data) {
 					postits = data;
 					callback();
 				});
 			},
 			//U
-			updatePostIt: function(postIt) {
+			updatePostIt: function (postIt) {
 				$http.put(URL + '/' + postIt.id, postIt);
 				console.log(postIt + ' was updated on the server');
 			},
 			//D
-			deletePostIt: function(postIt) {
+			deletePostIt: function (postIt) {
 				$http.delete(URL + '/' + postIt);
 				console.log(postIt + ' was deleted from the server');
 			},
 
-			getPostIts: function() {
+			getPostIts: function () {
 				// console.log(postits);
 				return postits;
 			}
