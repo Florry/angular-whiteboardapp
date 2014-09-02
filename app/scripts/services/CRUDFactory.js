65ac649ff7db627ea209d165a9d123a4d25be5ff
iPrytz <isak.prytz@gmail.com>
2014-09-02 13:42:24 +0200

    text in about

--- app/scripts/services/CRUDFactory.js
+++ app/scripts/services/CRUDFactory.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('whiteboardApp')
-	.factory('CRUDFactory', function ($http) {
+	.factory('CRUDFactory', function($http) {
 
 		// C POST OBJ TO URL http://api.beta2.se/wb-div-postits
 		// R GET OBJ FROM URL http://api.beta2.se/wb-div-postits
@@ -12,24 +12,24 @@ angular.module('whiteboardApp')
 
 		return {
 			//C
-			createPostIt: function (newPostIt, callback) {
-				$http.post(URL, newPostIt).success(function (data) {
+			createPostIt: function(newPostIt, callback) {
+				$http.post(URL, newPostIt).success(function(data) {
 					callback(data);
 				});
 			},
 			//R
-			readPostIts: function (callback) {
-				$http.get(URL + '/').success(function (data) {
+			readPostIts: function(callback) {
+				$http.get(URL + '/').success(function(data) {
 					callback(data);
 				});
 			},
 			//U
-			updatePostIt: function (postIt) {
+			updatePostIt: function(postIt) {
 				$http.put(URL + '/' + postIt.id, postIt);
 				console.log(postIt + ' was updated on the server');
 			},
 			//D
-			deletePostIt: function (postIt) {
+			deletePostIt: function(postIt) {
 				$http.delete(URL + '/' + postIt);
 				console.log(postIt + ' was deleted from the server');
 			}
