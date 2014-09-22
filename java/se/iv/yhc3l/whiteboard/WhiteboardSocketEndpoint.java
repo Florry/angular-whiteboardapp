c1733177797e6209415e3196d2c2f47ff7eec9f7
florry <soderstrom.viktor@gmail.com>
2014-09-22 11:32:36 +0200

    added java project

--- java/se/iv/yhc3l/whiteboard/WhiteboardSocketEndpoint.java
+++ java/se/iv/yhc3l/whiteboard/WhiteboardSocketEndpoint.java
@@ -1,6 +1,34 @@
 package se.iv.yhc3l.whiteboard;
 
+import javax.websocket.CloseReason;
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+
+import se.florry.yhc3l.websockets.ChatMessage;
+import se.florry.yhc3l.websockets.ChatMessageTextDecoder;
+import se.florry.yhc3l.websockets.ChatMessageTextEncoder;
+
+@ServerEndpoint(value = "/whiteboard")
 public class WhiteboardSocketEndpoint
 {	
+	@OnOpen
+	public void myOnOpen(Session session)
+	{}
+	
+	@OnMessage
+	public void myOnMessage(Session session, ChatMessage msg)
+	{}
+	
+	@OnClose
+	public void myOnClose(Session session, CloseReason reason)
+	{}
+	
+	@OnError
+	public void myOnError(Session session, Throwable throwable)
+	{}
 	
 }
