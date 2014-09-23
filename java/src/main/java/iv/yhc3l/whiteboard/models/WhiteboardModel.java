0cb55704d0c90f261a927a5304be53eaf38cb25f
iPrytz <isak.prytz@gmail.com>
2014-09-23 10:24:12 +0200

    whiteboardmodel.java created

---
+++ java/src/main/java/iv/yhc3l/whiteboard/models/WhiteboardModel.java
@@ -0,0 +1,52 @@
+package iv.yhc3l.whiteboard.models;
+
+import iv.yhc3l.whiteboard.utils.Utils;
+import java.util.ArrayList;
+
+
+public class WhiteboardModel {
+
+	int id;
+	String name;
+	String timestamp;
+	ArrayList<PostItModel> PostItModels;
+	
+	public WhiteboardModel(int id, String name) {
+		this.id = id;
+		this.name = name;
+		this.timestamp = Utils.getFormattedDate();
+		this.PostItModels = new ArrayList<PostItModel>();
+	}
+	
+	public WhiteboardModel(int id, String name, ArrayList<PostItModel> postItModels) {
+		this.id = id;
+		this.name = name;
+		this.timestamp = Utils.getFormattedDate();
+		this.PostItModels = postItModels;
+	}
+	
+	public WhiteboardModel(WhiteboardModel whiteboardModel){
+		this(whiteboardModel.id, whiteboardModel.name, whiteboardModel.PostItModels);
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getTimestamp() {
+		return timestamp;
+	}
+
+	public ArrayList<PostItModel> getPostItModels() {
+		return PostItModels;
+	}
+
+	public void addPostItModel(PostItModel postItModel) {
+		PostItModels.add(postItModel);
+		this.timestamp = Utils.getFormattedDate();
+	}
+}
